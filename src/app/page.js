625d3dbd2f0ca68d5dc79002b493836d370ddd69
Siam-AR <siam.ar.nexus@gmail.com>
2026-05-15 19:37:26 +0600

    feat: implement marquee

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
 import Banner from "@/components/Banner";
+import MarqueeComponent from "@/components/Marquee";
 import { discoverValidationDepths } from "next/dist/server/app-render/instant-validation/instant-validation";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
+        <MarqueeComponent></MarqueeComponent>
         <Banner></Banner>
     </div>
   );
